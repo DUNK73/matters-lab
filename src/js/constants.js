ea6e2e54bf92d41ae583f4cc222ff3c80b11309a
DUNK73 <z7dank3@yandex.ru>
2026-07-01 11:56:01 +0300

    next 34

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,3 @@
 //  const url = 'https://matters-backend-z7dunk3-5171s-projects.vercel.app';
-//  const url = 'https://matters-backend-lyart.vercel.app';
- const url = 'http://127.0.0.1:3002';
+ const url = 'https://matters-backend-lyart.vercel.app';
+//  const url = 'http://127.0.0.1:3002';
